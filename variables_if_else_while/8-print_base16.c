b7eaa6f2e8b61fd53ec976944da19f95a7ea0745
AysuAnar <8921@holbertonstudents.com>
2024-02-23 17:53:54 +0000

    task8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: 0 success
+ */
+int main(void)
+{
+int dig = '0';
+char chr = 'a';
+
+while (dig <= '9')
+{
+	putchar(dig);
+	dig++;
+}
+while (chr <= 'f')
+{
+	putchar(chr);
+	chr++;
+}
+	putchar('\n');
+	return (0);
+}
