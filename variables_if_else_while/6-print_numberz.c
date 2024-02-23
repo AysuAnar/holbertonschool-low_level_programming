36c12ecdadea619639b2bee0cb1a4cf53431d825
AysuAnar <8921@holbertonstudents.com>
2024-02-23 17:38:15 +0000

    task6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - prints all single digit numbers of base 10 starting from 0 
+ *
+ *Return: success
+ */
+
+int main(void)
+{
+int digit = '0';
+
+while (digit <= '9')
+{
+putchar(digit);
+digit++;
+}
+putchar('\n');
+return (0);
+}
