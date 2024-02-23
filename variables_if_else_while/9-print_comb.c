5264642224fe01d96b42f2b82d91043f63230cf4
AysuAnar <8921@holbertonstudents.com>
2024-02-23 17:55:19 +0000

    task9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: 0 success
+ */
+int main(void)
+{
+	int digit = '0';
+	while (digit <= '9')
+	{
+		putchar(digit);
+		if (digit == '9')
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
