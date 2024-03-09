0e2c39881f6e5a91816e6b47f2312db0f77a4de1
AysuAnar <8921@holbertonstudents.com>
2024-03-09 16:18:42 +0000

    task0

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -33,4 +33,15 @@ char *leet(char *);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
+int _putchar(char);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+int coincidence(char *a, char *b);
 #endif 
